43028335c084618fe350fea11579a29c8cd9766e
JoaoM10 <joao_m-4@hotmail.com>
2014-12-14 00:06:42 +0000

    starts semantic analysis

--- ast.cpp
+++ ast.cpp
@@ -1,4 +1,177 @@
 #include <bits/stdc++.h>
+#include "ast.h"
 using namespace std;
 
+map <string, VAL_TYPE> var_types;
+
+
+
+VAL_TYPE Value::get_value_type(){
+  if(tp == VT_VAR)
+    return var_types[get_var()];
+  return tp;
+}
+
+string Int::get_var(){
+  return "";
+}
+string Float::get_var(){
+  return "";
+}
+string Bool::get_var(){
+  return "";
+}
+string Var::get_var(){
+  return sval;
+}
+
+
+
+VAL_TYPE Exp_Val::get_type(){
+  return v1->get_value_type();
+}
+VAL_TYPE Exp_Add::get_type(){
+  VAL_TYPE tp1 = x1->get_type();
+  VAL_TYPE tp2 = x2->get_type();
+  if(tp1 == VT_FLOAT || tp2 == VT_FLOAT)
+    return VT_FLOAT;
+  return VT_INT;
+}
+VAL_TYPE Exp_Sub::get_type(){
+  VAL_TYPE tp1 = x1->get_type();
+  VAL_TYPE tp2 = x2->get_type();
+  if(tp1 == VT_FLOAT || tp2 == VT_FLOAT)
+    return VT_FLOAT;
+  return VT_INT;
+}
+VAL_TYPE Exp_Mul::get_type(){
+  VAL_TYPE tp1 = x1->get_type();
+  VAL_TYPE tp2 = x2->get_type();
+  if(tp1 == VT_FLOAT || tp2 == VT_FLOAT)
+    return VT_FLOAT;
+  return VT_INT;
+}
+VAL_TYPE Exp_Div::get_type(){
+  VAL_TYPE tp1 = x1->get_type();
+  VAL_TYPE tp2 = x2->get_type();
+  if(tp1 == VT_FLOAT || tp2 == VT_FLOAT)
+    return VT_FLOAT;
+  return VT_INT;
+}
+VAL_TYPE Exp_Pow::get_type(){
+  VAL_TYPE tp1 = x1->get_type();
+  VAL_TYPE tp2 = x2->get_type();
+  if(tp1 == VT_FLOAT || tp2 == VT_FLOAT)
+    return VT_FLOAT;
+  return VT_INT;
+}
+VAL_TYPE Exp_Mod::get_type(){
+  VAL_TYPE tp1 = x1->get_type();
+  VAL_TYPE tp2 = x2->get_type();
+  if(tp1 == VT_FLOAT || tp2 == VT_FLOAT)
+    return VT_FLOAT;
+  return VT_INT;
+}
+VAL_TYPE Exp_Min::get_type(){
+  return v1->get_value_type();
+}
+VAL_TYPE Exp_And::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Or::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Eq::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Neq::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Lt::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Gt::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Leq::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Geq::get_type(){
+  return VT_BOOL;
+}
+VAL_TYPE Exp_Not::get_type(){
+  return VT_BOOL;
+}
+
+
+bool Exp_Val::semantic_analysis(){
+  return true;
+}
+bool Exp_Add::semantic_analysis(){
+  return true;
+}
+bool Exp_Sub::semantic_analysis(){
+  return true;
+}
+bool Exp_Mul::semantic_analysis(){
+  return true;
+}
+bool Exp_Div::semantic_analysis(){
+  return true;
+}
+bool Exp_Pow::semantic_analysis(){
+  return true;
+}
+bool Exp_Mod::semantic_analysis(){
+  return true;
+}
+bool Exp_Min::semantic_analysis(){
+  return true;
+}
+bool Exp_And::semantic_analysis(){
+  return true;
+}
+bool Exp_Or::semantic_analysis(){
+  return true;
+}
+bool Exp_Eq::semantic_analysis(){
+  return true;
+}
+bool Exp_Neq::semantic_analysis(){
+  return true;
+}
+bool Exp_Lt::semantic_analysis(){
+  return true;
+}
+bool Exp_Gt::semantic_analysis(){
+  return true;
+}
+bool Exp_Leq::semantic_analysis(){
+  return true;
+}
+bool Exp_Geq::semantic_analysis(){
+  return true;
+}
+bool Exp_Not::semantic_analysis(){
+  return true;
+}
+
+
+
+bool Cmd_Atr::semantic_analysis(){
+  var_types[v1->get_var()] = x1->get_type();
+  return true;
+}
+bool Cmd_Seq::semantic_analysis(){
+  return true;
+}
+bool Cmd_Print::semantic_analysis(){
+  return true;
+}
+bool Cmd_While::semantic_analysis(){
+  return (x1->get_type() == VT_BOOL);
+}
+bool Cmd_If::semantic_analysis(){
+  return (x1->get_type() == VT_BOOL);
+}
 
--- ast.h
+++ ast.h
@@ -7,26 +7,32 @@ enum VAL_TYPE { VT_INT, VT_FLOAT, VT_BOOL, VT_VAR };
 class Value {
 public:
   VAL_TYPE tp;
+  VAL_TYPE get_value_type();
+  virtual string get_var() = 0;
 };
 class Int : public Value {
 public:
   int ival;
-  Int (int x) : ival(x) { tp = VT_INT; }  
+  Int (int x) : ival(x) { tp = VT_INT; }
+  string get_var();
 };
 class Float : public Value {
 public:
   double fval;
-  Float (double x) : fval(x) { tp = VT_FLOAT; }  
+  Float (double x) : fval(x) { tp = VT_FLOAT; }
+  string get_var();
 };
 class Bool : public Value {
 public:
   bool bval;
   Bool (bool x) : bval(x) { tp = VT_BOOL; }  
+  string get_var();
 };
 class Var : public Value {
 public:
   string sval;
-  Var (char *x) : sval(x) { tp = VT_VAR; }  
+  Var (char *x) : sval(x) { tp = VT_VAR; }
+  string get_var();
 };
 
 
@@ -39,77 +45,113 @@ public:
   Value *v1;
   Exp (EXP_TYPE t, Exp *p1, Exp *p2) : tp(t), x1(p1), x2(p2) { }
   Exp (EXP_TYPE t, Value *z1) : tp(t), v1(z1) { }
+  virtual VAL_TYPE get_type() = 0;
+  virtual bool semantic_analysis() = 0;
 };
 // Variables...
 class Exp_Val : public Exp {
 public:
   Exp_Val (Value *v) : Exp(ET_VAL, v) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 // Numeric Expressions...
 class Exp_Add : public Exp {
 public:
   Exp_Add (Exp *p1, Exp *p2) : Exp(ET_ADD, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Sub : public Exp {
 public:
   Exp_Sub (Exp *p1, Exp *p2) : Exp(ET_SUB, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Mul : public Exp {
 public:
   Exp_Mul (Exp *p1, Exp *p2) : Exp(ET_MUL, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Div : public Exp {
 public:
   Exp_Div (Exp *p1, Exp *p2) : Exp(ET_DIV, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Pow : public Exp {
 public:
   Exp_Pow (Exp *p1, Exp *p2) : Exp(ET_POW, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Mod : public Exp {
 public:
   Exp_Mod (Exp *p1, Exp *p2) : Exp(ET_MOD, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Min : public Exp {
 public:
   Exp_Min (Value *v) : Exp(ET_MIN, v) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 // Boolean Expressions...
 class Exp_And : public Exp {
 public:
   Exp_And (Exp *p1, Exp *p2) : Exp(ET_AND, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Or : public Exp {
 public:
   Exp_Or (Exp *p1, Exp *p2) : Exp(ET_OR, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Eq : public Exp {
 public:
   Exp_Eq (Exp *p1, Exp *p2) : Exp(ET_EQ, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Neq : public Exp {
 public:
   Exp_Neq (Exp *p1, Exp *p2) : Exp(ET_NEQ, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Lt : public Exp {
 public:
   Exp_Lt (Exp *p1, Exp *p2) : Exp(ET_LT, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Gt : public Exp {
 public:
   Exp_Gt (Exp *p1, Exp *p2) : Exp(ET_GT, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Leq : public Exp {
 public:
   Exp_Leq (Exp *p1, Exp *p2) : Exp(ET_LEQ, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Geq : public Exp {
 public:
   Exp_Geq (Exp *p1, Exp *p2) : Exp(ET_GEQ, p1, p2) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 class Exp_Not : public Exp {
 public:
   Exp_Not (Exp *p1) : Exp(ET_NOT, p1, NULL) { }
+  VAL_TYPE get_type();
+  bool semantic_analysis();
 };
 
 
@@ -133,28 +175,34 @@ public:
   Lst *lst;
   ElseIf *el;
   Cmd (CMD_TYPE t) : tp(t) { }
+  virtual bool semantic_analysis() = 0;
 };
 class Cmd_Atr : public Cmd {
 public:
-  Cmd_Atr (Var *z, Exp *p1) : Cmd(CT_ATR) { v1 = z; x1 = p1; }
+  Cmd_Atr (Var *z, Exp *p1) : Cmd(CT_ATR) { v1 = z; x1 = p1; semantic_analysis(); }
+  bool semantic_analysis();
 };
 class Cmd_Seq : public Cmd {
 public:
-  Cmd_Seq (Cmd *r1, Cmd *r2) : Cmd(CT_SEQ) { c1 = r1; c2 = r2; } 
+  Cmd_Seq (Cmd *r1, Cmd *r2) : Cmd(CT_SEQ) { c1 = r1; c2 = r2; semantic_analysis(); } 
+  bool semantic_analysis();
 };
 class Cmd_Print : public Cmd {
 public:
-  Cmd_Print (Lst *ls) : Cmd(CT_PRINT) { lst = ls; } 
+  Cmd_Print (Lst *ls) : Cmd(CT_PRINT) { lst = ls; semantic_analysis(); }
+  bool semantic_analysis();
 };
 class Cmd_While : public Cmd {
 public:
-  Cmd_While (Exp *p1, Cmd *r1) : Cmd(CT_WHILE) { x1 = p1; c1 = r1; } 
+  Cmd_While (Exp *p1, Cmd *r1) : Cmd(CT_WHILE) { x1 = p1; c1 = r1; semantic_analysis(); }
+  bool semantic_analysis();
 };
 class Cmd_If : public Cmd {
 public:
-  Cmd_If (Exp *p1, Cmd *r1) : Cmd(CT_IF) { x1 = p1; c1 = r1; el = NULL; }
-  Cmd_If (Exp *p1, Cmd *r1, ElseIf *ei) : Cmd(CT_IF) { x1 = p1; c1 = r1; el = ei; } 
-  Cmd_If (Exp *p1, Cmd *r1, ElseIf *ei, Cmd *r2) : Cmd(CT_IF) { x1 = p1; c1 = r1; el = ei; c2 = r2; } 
+  Cmd_If (Exp *p1, Cmd *r1) : Cmd(CT_IF) { x1 = p1; c1 = r1; el = NULL; semantic_analysis(); }
+  Cmd_If (Exp *p1, Cmd *r1, ElseIf *ei) : Cmd(CT_IF) { x1 = p1; c1 = r1; el = ei; semantic_analysis(); } 
+  Cmd_If (Exp *p1, Cmd *r1, ElseIf *ei, Cmd *r2) : Cmd(CT_IF) { x1 = p1; c1 = r1; el = ei; c2 = r2; semantic_analysis(); }
+  bool semantic_analysis();
 };
 
 
